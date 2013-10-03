8cdbd38306f1813d4c5a6e08deb9686923ab995d
Don Coleman <dcoleman@chariotsolutions.com>
2013-10-02 21:48:06 -0400

    wp8 platform

---
+++ platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.dialogs/www/notification.js",
+        "id": "org.apache.cordova.core.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.vibration/www/vibration.js",
+        "id": "org.apache.cordova.core.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+]
+});
